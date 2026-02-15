976ac048b5168e30ab6eadcd0ea79102fd5f72dd
chai-aur-react <shambharkaranuj@gmail.com>
2026-02-15 09:21:26 +0530

    add landing page on ipo page

--- frontend/src/layout/IpoLayout.jsx
+++ frontend/src/layout/IpoLayout.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import UpcomingIpos from '../pages/UpcomingIpos'
 
 const IpoLayout = () => {
+  const location=useLocation();
   return (
-    <div>
+    <div style={{padding:"40px"}}>
+      {
+        location.pathname==="/ipo" && (
+          <div>
+            <h2>IPO Categories</h2>
+            <ul style={{lineHeight:"2.5"}}>
+              <li><Link to="upcoming-ipo">Upcoming Ipos</Link></li>
+              <li><Link to="ongoing-ipo">Ongoing Ipos</Link></li>
+              <li><Link to="listed-ipo">Listed Ipos</Link></li>
+            </ul>
+          </div>
+        )
+      }
       <Outlet/>
     </div>
   )
